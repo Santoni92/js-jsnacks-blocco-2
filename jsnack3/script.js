40cd9a0478fd23da0383cc9718b34b1e54549e1c
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-11 18:39:38 +0100

    modifica

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -3,14 +3,25 @@
 
   console.log('JS OK!');
 
-  let array1 = [1,2,3,4,5,6];
-  let array2 = [1,2];
-
-  let lunghezzaArray1 = array1.length;
-  let lunghezzaArray2 = array2.length;
+  //let array1 = [1,2,3,4,5,6];
+  //let array2 = [1,2];
+  // let lunghezzaArray1 = array1.length;
+ //let lunghezzaArray2 = array2.length;
+const array1 = [];
+const array2 = [];
+lunghezzaArray1 = Math.floor(Math.random() * 10);   //inizializzo con un numero random la lunghezza del primo array
+lunghezzaArray2 = Math.floor(Math.random() * 10);   //inizializzo con un numero random la lunghezza del secondo array
+for(let i = 0; i < lunghezzaArray1; i++){
+    array1.push(Math.floor(Math.random() * 10));    //inizializzo il primo array con numeri random generati nel range 0-10
+}
+for(let i = 0; i < lunghezzaArray2; i++){
+    array2.push(Math.floor(Math.random() * 10));
+}
+ 
 
 console.log('Il primo array é: ' + array1);
 console.log('Il secondo array é: ' + array2);
+
 
   if(lunghezzaArray1 > lunghezzaArray2)
   {
@@ -21,9 +32,9 @@ console.log('Il secondo array é: ' + array2);
                                                                     elementi pari alla differenza tra le rispettive quantità di elementi)*/
                                                                     
     {
-        array2.push(i); //non creo elementi random ma semplicemento aggiungo all'array2 il valore del contatore del ciclo
+        array2.push(i); //non creo elementi random (non mi va) ma semplicemento aggiungo all'array2 il valore del contatore del ciclo
     }
-    console.log("Dopo l'aggiunta di elementi al secondo array, i due array hanno la stessa quantità di elementi" );
+    console.log("Dopo l'aggiunta di elementi al secondo array, i due array vengono ad avere la stessa quantità di elementi" );
     console.log('Array 1 risulta: ' + array1);
     console.log('Array 2 risulta: ' + array2);
   }else if(lunghezzaArray2 > lunghezzaArray1){
@@ -32,7 +43,7 @@ console.log('Il secondo array é: ' + array2);
     {
         array1.push(i);
     }
-    console.log("Dopo l'aggiunta di elementi al primo array, i due array hanno la stessa quantità di elementi" );
+    console.log("Dopo l'aggiunta di elementi al primo array, i due array vengono ad avere la stessa quantità di elementi" );
     console.log('Array 1 risulta: ' + array1);
     console.log('Array 2 risulta: ' + array2);
 
