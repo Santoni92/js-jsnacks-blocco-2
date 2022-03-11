c93fc76981562c310688074c06dcb4582984ad2e
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-11 17:58:12 +0100

    implementazione col for;ho posto l'incremento del contatore all'interno dell'if cosicchè solo quando effettivamente inserisco un numero il conteggio va avanti

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,9 +2,11 @@
   inseriti. Esegui questo programma in due versioni, con il for e con il while.*/ 
 
   console.log('JS OK!');
+  /*****************************************implementazione col while***************************************************/
+  /*
   let indice = 0;
   let somma = 0;
-  while(indice < 4)
+  while(indice < 5)
   {
     let numeroInserito;
       while(isNaN(numeroInserito))
@@ -15,3 +17,35 @@
      somma += numeroInserito;
   }
   console.log('La somma dei numeri inseriti risulta: ' + somma);
+*/
+
+let indice = 0;
+let somma = 0;
+while(indice < 5){
+    const numeroInserito = parseInt(prompt('Inserisci numero: '));
+    if(!isNaN(numeroInserito))
+    {
+        somma += numeroInserito;
+        indice++;
+    }
+}
+console.log('La somma dei numeri inseriti risulta: ' + somma);
+
+  /***********************************************implementazione con il for**********************************************/
+  /*
+  let somma = 0;
+  for(let i = 0; i < 5; ){
+      const numeroInserito = parseInt(prompt('inserisci numero: '));
+      if(!isNaN(numeroInserito))
+      {
+          somma += numeroInserito;
+          i++;  //quando il numero inserito è effettivamente un numero allora aggiorno la somma ed incremento il contatore così da far terminare il ciclo
+      }
+  }
+  console.log('La somma dei numeri inseriti risulta: ' + somma);
+*/
+
+
+
+
+  
